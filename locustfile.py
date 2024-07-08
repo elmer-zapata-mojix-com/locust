5b318136e54286a5ad3cd6b9a079ea1ca857f686
Elmer Zapata <elmer.765@gmail.com>
2024-07-08 15:40:25 -0400

    add wait time

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,7 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class PokemonExample(HttpUser):
+    wait_time = between(1, 5)
     @task
     def hello_world(self):
         self.client.get("/api/v2/pokemon/onix")
